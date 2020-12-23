6ad9ec7215ebea17a931740b078ba23b1b3ac614
Lukas Mat <lukas@smoolabs.com>
2020-12-23 16:35:16 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,124 @@
+let nowPlayingHash = null;
+
+const size = getQueryVariable('size') || '100';
+const color = getQueryVariable('color') || 'black';
+
+const css = document.createElement('style');
+css.innerHTML = `html, body { font-size: ${size}%; color: ${color}; }`;
+document.body.append(css);
+
+setInterval(tick, 2000);
+tick();
+
+async function tick() {
+	const nowPlaying = await fetchNowPlaying();
+
+	if (nowPlaying && hashString(nowPlaying.title) !== nowPlayingHash) {
+		nowPlayingHash = hashString(nowPlaying.title);
+
+		updateTrack(nowPlaying);
+
+		const coverId = await fetchCoverForTrackJson(nowPlaying);
+
+		if (coverId)
+			updateCover(coverId);
+	}
+}
+
+
+async function fetchNowPlaying() {
+	try {
+		const response = await fetch('/example.json');
+		const json = await response.json();
+
+		return json;
+	} catch (e) {
+		console.error('Error fetching now playing', e);
+		return null;
+	}
+}
+
+async function updateTrack(trackJson) {
+	document.querySelector('.title').textContent = trackJson.title;
+	document.querySelector('.artist').textContent = trackJson.artist;
+	
+	document.querySelector('.cover').src = 'default.jpg';
+}
+
+async function updateCover(coverId) {
+	const coverDOM = document.querySelector('.cover');
+	
+	// coverDOM.classList.remove('hidden');
+	coverDOM.src = `https://coverartarchive.org/release/${coverId}/front`;
+}
+
+async function fetchCoverForTrackJson(trackJson) {
+	const ids = await fetchReleaseIdsOnMusicBrainz(trackJson.title, trackJson.artist);
+
+	if (ids.length === 0) {
+		throw new Error('track not found');
+	}
+
+	const coverId = await fetchCoverForReleaseIds(ids);
+
+	return coverId;
+}
+
+async function fetchReleaseIdsOnMusicBrainz(title, artist) {
+	const artistQuery = artist
+		? `AND artist:"${encodeURIComponent(artist)}"`
+		: '';
+	const url = `https://musicbrainz.org/ws/2/recording?query=title:"${encodeURIComponent(title)}"${artistQuery}&fmt=json`;
+
+	let ids = [];
+
+	try {
+		const response = await fetch(url);
+		const json = await response.json();
+
+		if (json && json.recordings && json.recordings.length > 0) {
+			for (const recording of json.recordings) {
+				if (!recording.releases)
+					continue;
+
+				for (const release of recording.releases) {
+					ids.push(release.id);
+				}
+			}
+
+			console.log('Found', ids.length, 'releases');
+
+			return ids;
+		} else {
+			console.warn('No releases found 404');
+			return [];
+		}
+	} catch (e) {
+		console.error('Error fetching song from MusicBrainz', e);
+		return [];
+	}
+}
+
+async function fetchCoverForReleaseIds(releaseIds) {
+	const chunks = chunkArray(releaseIds, 2);
+	for (const idChunk of chunks) {
+		try {
+			const id = await Promise.any(idChunk.map(id => checkCover(id)));
+			return id;
+		} catch (e) {
+			// nothin
+		}
+	}
+
+	return null;
+}
+
+async function checkCover(id) {
+	const response = await fetch('https://coverartarchive.org/release/' + id);
+		
+	if (response.status < 400) {
+		return id;
+	}
+
+	throw new Error('cover not found');
+}
---
+++ helpers.js
@@ -0,0 +1,36 @@
+function chunkArray(array, groupsize){
+    var sets = [], chunks, i = 0;
+    chunks = Math.ceil(array.length / groupsize);
+
+    while(i < chunks){
+        sets[i] = array.splice(0, groupsize);
+	i++;
+    }
+    return sets;
+};
+
+function hashString(str) {
+    var hash = 0;
+    if (str.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < str.length; i++) {
+        var char = str.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    
+    return null;
+}
